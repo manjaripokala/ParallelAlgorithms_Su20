ab302a1e3132d7f856fe64a7d9454aef84b77b72
dpedz-tech <dpedzinski@pedz-tech.com>
2020-07-25 14:00:17 -0500

    weird issue on part B for problem 1

--- hw3/Array.c
+++ hw3/Array.c
@@ -7,11 +7,29 @@ typedef struct {
     size_t size;
 } Array;
 
+//typedef struct {
+//    char **array;
+//    size_t used;
+//    size_t size;
+//} CharArray;
+
+//typedef struct {
+//    char **array;
+//    size_t used;
+//    size_t size;
+//} CharArray;
+
 void initArray(Array *a, size_t initialSize) {
     a->array = (int*) malloc(initialSize * sizeof(int));
     a->used = 0;
     a->size = initialSize;
 }
+//
+//void initCharArray(CharArray *a, size_t initialSize) {
+//    a->array = (char**) malloc(initialSize * sizeof(char[10]));
+//    a->used = 0;
+//    a->size = initialSize;
+//}
 
 void insertArray(Array *a, int element) {
     // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
@@ -23,17 +41,27 @@ void insertArray(Array *a, int element) {
     a->array[a->used++] = element;
 }
 
-void freeArray(Array *a) {
-    free(a->array);
-    a->array = NULL;
-    a->used = a->size = 0;
-}
+//void insertCharArray(CharArray *a, char* element) {
+//    // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+//    // Therefore a->used can go up to a->size
+//    if (a->used == a->size) {
+//        a->size *= 2;
+//        a->array = (char**) realloc(a->array, a->size * sizeof(char[10]));
+//    }
+//    a->array[a->used++] = element;
+//}
+//
+////void freeIntArray(IntArray *a) {
+////    free(a->array);
+////    a->array = NULL;
+////    a->used = a->size = 0;
+////}
 
 Array initArrayA(){
     FILE *fp;
     char str[50000];
     Array a;
-    initArray(&a, 100);  // initially 5 elements
+    initArray(&a, 100);
 
     /* opening file for reading */
     fp = fopen("inp.txt" , "r");
@@ -53,6 +81,31 @@ Array initArrayA(){
     fclose(fp);
     return a;
 }
+//
+//CharArray initCharArrayA(){
+//    FILE *fp;
+//    char str[50000];
+//    CharArray a;
+//    initCharArray(&a, 100);
+//
+//    /* opening file for reading */
+//    fp = fopen("inp.txt" , "r");
+//    if(fp == NULL) {
+//        printf("%s","error");
+//        return a;
+//    }
+//    if( fgets (str, 50000, fp)!=NULL ) {
+//        /* writing content to stdout */
+////        printf("%s\n", str);
+//        char* token;
+//        char* rest = str;
+//
+//        while ((token = strtok_r(rest, " , ", &rest)))
+//            insertCharArray(&a, token);
+//    }
+//    fclose(fp);
+//    return a;
+//}
 
 
 
--- hw3/hw3-problem1_b.cu
+++ hw3/hw3-problem1_b.cu
@@ -1,100 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <cuda_runtime.h>
 #include "Array.c"
 
-__global__ void global_reduce_kernel(int * d_out, int * d_in)
-{
-    int myId = threadIdx.x + blockDim.x * blockIdx.x;
-    int tid  = threadIdx.x;
+// Find the first digit https://www.geeksforgeeks.org/find-first-last-digits-number/
+//int firstDigit(int n)
+//{
+//    // Remove last digit from number
+//    // till only one digit is left
+//    while (n >= 10)
+//        n /= 10;
+//
+//    // return the first digit
+//    return n;
+//}
+//
+//// Find the last digit
+//int lastDigit(int n)
+//{
+//    // return the last digit
+//    return (n % 10);
+//}
 
-    // do reduction in global mem
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-    {
-        if (tid < s)
-        {
-            if ( d_in[myId] < d_in[myId + s]){
-                d_in[myId]= d_in[myId + s];
-            }
-        }
-        __syncthreads();        // make sure all adds at one stage are done!
-    }
-
-    // only thread 0 writes result for this block back to global mem
-    if (tid == 0)
-    {
-        d_out[blockIdx.x] = d_in[myId];
-    }
+__global__ void getlastdigit(int * d_out, int * d_in){
+    int idx = threadIdx.x;
+//    int x =;
+    d_out[idx] = ( d_in[idx] % 10);
 }
 
-__global__ void shmem_reduce_kernel(int * d_out, const int * d_in)
-{
-    // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
-    extern __shared__ float sdata[];
+int main(int argc, char ** argv) {
 
-    int myId = threadIdx.x + blockDim.x * blockIdx.x;
-    int tid  = threadIdx.x;
-
-    // load shared mem from global mem
-    sdata[tid] = d_in[myId];
-    __syncthreads();            // make sure entire block is loaded!
-
-    // do reduction in shared mem
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-    {
-        if (tid < s)
-        {
-            if ( sdata[tid] < sdata[tid + s]){
-                sdata[tid]= sdata[tid + s];
-            }
-        }
-        __syncthreads();        // make sure all adds at one stage are done!
-    }
-
-    // only thread 0 writes result for this block back to global mem
-    if (tid == 0)
-    {
-        d_out[blockIdx.x] = sdata[0];
-    }
-}
-
-void reduce(int * d_out, int * d_intermediate, int * d_in,
-            int size, bool usesSharedMemory)
-{
-    // assumes that size is not greater than maxThreadsPerBlock^2
-    // and that size is a multiple of maxThreadsPerBlock
-    const int maxThreadsPerBlock = 512;
-    int threads = maxThreadsPerBlock;
-    int blocks = size / maxThreadsPerBlock;
-    if (usesSharedMemory)
-    {
-        shmem_reduce_kernel<<<blocks, threads, threads * sizeof(int)>>>
-                (d_intermediate, d_in);
-    }
-    else
-    {
-        global_reduce_kernel<<<blocks, threads>>>
-                (d_intermediate, d_in);
-    }
-
-    // now we're down to one block left, so reduce it
-    threads = blocks; // launch one thread for each block in prev step
-    blocks = 1;
-    if (usesSharedMemory)
-    {
-        shmem_reduce_kernel<<<blocks, threads, threads * sizeof(int)>>>
-                (d_out, d_intermediate);
-    }
-    else
-    {
-        global_reduce_kernel<<<blocks, threads>>>
-                (d_out, d_intermediate);
-    }
-}
-
-int main(int argc, char **argv)
-{
-    printf("here");
     int deviceCount;
     cudaGetDeviceCount(&deviceCount);
     if (deviceCount == 0) {
@@ -117,65 +51,47 @@ int main(int argc, char **argv)
     int * h_in = A.array;
     const int ARRAY_SIZE = A.size;
     const int ARRAY_BYTES = A.size * sizeof(int);
+    int h_out[ARRAY_SIZE];
 
-    // generate the input array on the host
-    int max = 0;
     printf("array size is %d\n", ARRAY_SIZE);
+//    clock_t t;
+//    t = clock();
 
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        if (max <= h_in[i]){
-            max = h_in[i];
-        }
+    printf("%s\n", "Input[100]:");
+
+    for(int i = 0; i < 200; i++) {
+        printf("%d, ",(h_in[i]% 10));
     }
-    printf("Max at host: %d\n", max);
+    printf("\n %s\n", "Values[100]:");
+
+//    t = clock() - t;
+//    double time_taken = ((double)t)/(CLOCKS_PER_SEC/1000); // calculate the elapsed time
+//    printf("The host took %f ms to execute\n", time_taken);
+//    printf("Max at host: %d\n", max);
 
     // declare GPU memory pointers
-    int * d_in, * d_intermediate, * d_out;
+    int * d_in;
+    int * d_out;
 
     // allocate GPU memory
-    cudaMalloc((void **) &d_in, ARRAY_BYTES);
-    cudaMalloc((void **) &d_intermediate, ARRAY_BYTES); // overallocated
-    cudaMalloc((void **) &d_out, sizeof(int));
+    cudaMalloc((void**) &d_in, ARRAY_BYTES);
+    cudaMalloc((void**) &d_out, ARRAY_BYTES);
 
-    // transfer the input array to the GPU
+    // transfer the array to the GPU
     cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    // launch the kernel
+    getlastdigit<<<1, ARRAY_SIZE>>>(d_out, d_in);
 
-    cudaEvent_t start2, stop2;
-    cudaEventCreate(&start2);
-    cudaEventCreate(&stop2);
+    // copy back the result array to the CPU
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
-    printf("Running global reduce\n");
-    cudaEventRecord(start, 0);
-    reduce(d_out, d_intermediate, d_in, ARRAY_SIZE, false);
-    cudaEventRecord(stop, 0);
-
-    cudaEventSynchronize(stop);
-    float elapsedTime;
-    cudaEventElapsedTime(&elapsedTime, start, stop);
-    int h_out;
-    cudaMemcpy(&h_out, d_out, sizeof(int), cudaMemcpyDeviceToHost);
-    printf("average time elapsed: %f\n", elapsedTime);
-    printf("Max returned by device: %d\n", h_out);
-
-    printf("Running reduce with shared mem\n");
-    cudaEventRecord(start2, 0);
-    reduce(d_out, d_intermediate, d_in, ARRAY_SIZE, true);
-    cudaEventRecord(stop2, 0);
-    cudaEventSynchronize(stop2);
-    float elapsedTime2;
-    cudaEventElapsedTime(&elapsedTime2, start2, stop2);
-    int h_out2;
-    cudaMemcpy(&h_out2, d_out, sizeof(int), cudaMemcpyDeviceToHost);
-    printf("average time elapsed: %f\n", elapsedTime2);
-    printf("Max returned by device: %d\n", h_out2);
-
-    // free GPU memory allocation
+    // print out the resulting array
+    for (int i =0; i < 200; i++) {
+        printf("%d, ", h_out[i]);
+    }
+    printf("\n%s", "");
     cudaFree(d_in);
-    cudaFree(d_intermediate);
     cudaFree(d_out);
 
     return 0;
