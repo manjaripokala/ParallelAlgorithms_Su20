0220c115689bfd1f8b6478b079f4f5d5afce397f
Manjari Pokala <manjaripokala@Manjaris-MacBook-Air.local>
2020-08-13 19:03:56 -0500

    cleanup

--- hw1/src/hw1.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-
-// Returns the euclidian lenght of 'vector'.
-double euclidean_length(std::vector<double> vector);
-
-// Returns a sorted vector that contains all the unique elements in 'sorted_vector'.
-//
-// NOTE: non-const vectors are not thread-safe.
-std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector);
